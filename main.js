42a816a57bf219540b3f5ec516c8994026db89c2
Duncan G <duncan.gabel@gmail.com>
2024-06-20 15:13:41 -0500

    Added to files

---
+++ main.js
@@ -0,0 +1,7 @@
+const words = [Apple, Balloon, Banana, Basket, Beach, Bear, Bee, Bicycle, Book, Bottle, Bridge, Butterfly, Cake, Candle, Car, Cat, Chair, Cheese, Clock, Cloud, Computer, Cup, Dog, Door, Dragonfly, Elephant, Fish, Flower, Forest, Fruit, Garden, Glass, Grass, Hat, House, Ice, Island, Juice, Kite, Lamp, Leaf, Moon, Mountain, Mouse, Ocean, Pencil, River, Snow, Star, Tree]
+let generatedWords = [];
+const button = document.querySelector("button");
+const generateWords = () => {
+    
+
+};
