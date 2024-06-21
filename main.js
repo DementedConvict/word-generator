f7d6d77e56b36b520db1a7a39dd7ddafe7c5d0c9
Duncan G <duncan.gabel@gmail.com>
2024-06-20 19:19:30 -0500

    Added functionality to random word generator

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
-const words = [Apple, Balloon, Banana, Basket, Beach, Bear, Bee, Bicycle, Book, Bottle, Bridge, Butterfly, Cake, Candle, Car, Cat, Chair, Cheese, Clock, Cloud, Computer, Cup, Dog, Door, Dragonfly, Elephant, Fish, Flower, Forest, Fruit, Garden, Glass, Grass, Hat, House, Ice, Island, Juice, Kite, Lamp, Leaf, Moon, Mountain, Mouse, Ocean, Pencil, River, Snow, Star, Tree]
+const wordsOriginal = ["a", "about", "afternoon", "and", "apple", "are", "bananas", "birds", "blue", "book", "busy", "by", "cat", "city", "clouds", "coffee", "cold", "comes", "dark", "day", "do", "dogs", "down", "early", "eat", "evening", "flies", "flowers", "for", "from", "garden", "get", "goes", "good", "green", "have", "here", "house", "in", "is", "it", "jump", "just", "lamp", "late", "light", "like", "little", "morning", "mouse", "music", "my", "night", "of", "old", "on", "orange", "outside", "people", "play", "rain", "red", "room", "run", "see", "sings", "sky", "sleep", "small", "snow", "stars", "sun", "sweet", "swim", "takes", "tea", "the", "their", "this", "time", "to", "tree", "under", "up", "very", "walk", "water", "we", "when", "white", "with", "yellow", "you"
+];
 let generatedWords = [];
-const button = document.querySelector("button");
+//const button = document.querySelector("button");
 const generateWords = () => {
-    
-
+    let generate = Math.floor(Math.random() * wordsOriginal.length);
+    console.log(generate);
+    for (let i = 0; i < generate; i++) {
+        let generation = Math.floor(Math.random() * wordsOriginal.length);
+        let word = wordsOriginal[generation];
+        generatedWords.push(word);
+    };
 };
+generateWords();
+console.log(generatedWords.join(' '));
